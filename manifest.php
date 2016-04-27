81113b8a7d0bc234127e723a7ad43d7f31ffd786
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-04-27 14:47:22 +0200

    reformat composer and manifest

--- manifest.php
+++ manifest.php
@@ -1,23 +1,23 @@
 <?php
-/**  
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
  * of the License (non-upgradable).
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
+ *
  * Copyright (c) 2016 (original work) Open Assessment Technologies SA;
- *               
- * 
- */               
+ *
+ *
+ */
 
 return array(
     'name' => 'itemqtiCreator',
@@ -39,16 +39,15 @@ return array(
     ),
     'routes' => array(
         '/itemqtiCreator' => 'oat\\itemqtiCreator\\controller'
-    ),    
-	'constants' => array(
-	    # views directory
-	    "DIR_VIEWS" => dirname(__FILE__).DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
-	    
-		#BASE URL (usually the domain root)
-		'BASE_URL' => ROOT_URL.'itemqtiCreator/',
-	    
-	    #BASE WWW required by JS
-	    'BASE_WWW' => ROOT_URL.'itemqtiCreator/views/'
-	),
+    ),
+    'constants' => array(
+        # views directory
+        "DIR_VIEWS" => dirname(__FILE__).DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
+
+        #BASE URL (usually the domain root)
+        'BASE_URL' => ROOT_URL.'itemqtiCreator/',
+
+        #BASE WWW required by JS
+        'BASE_WWW' => ROOT_URL.'itemqtiCreator/views/'
     )
 );
